a68dbec9f02d2c3698626d9b583bc78abe1aea66
harsh-05 <harshverma09022001@gmail.com>
2025-11-03 19:39:43 +0530

    Added the hidden card inside card

--- src/Components/AnimatedCard.tsx
+++ src/Components/AnimatedCard.tsx
@@ -1,8 +1,147 @@
+import {easeInOut, motion} from 'motion/react'
 export default function AnimatedCard() {
-    return (
-      <div className="bg-white rounded-xl min-h-104 h-104 w-72 font-sans shadow-[0_1px_1px_rgba(0,0,0,0.05),0_4px_6px_rgba(34,42,53,0.04),0_24px_68px_rgba(47,48,55,0.05),0_2px_3px_rgba(0,0,0,0.04)] p-4 flex flex-col ">
-        <h5 className="text-[10px] font-bold">Aceternity UI Components</h5>
-        <p className="text-[10px] mt-2 text-neutral-400  font-sans">A collection of UI components for your project, get on with it.</p>
+
+  return (
+    <motion.div className="bg-white rounded-xl min-h-104 h-104 w-72 font-sans shadow-[0_1px_1px_rgba(0,0,0,0.05),0_4px_6px_rgba(34,42,53,0.04),0_24px_68px_rgba(47,48,55,0.05),0_2px_3px_rgba(0,0,0,0.04)] p-4 flex flex-col ">
+      <h5 className="text-sm font-bold">Aceternity UI Components</h5>
+      <p className="text-xs mt-2 text-neutral-400  font-sans">
+        A collection of UI components for your project, get on with it.
+      </p>
+      <div className="flex items-center justify-center mt-7 ">
+        <button className="flex items-end justify-center gap-2 text-sm rounded-md shadow-[0_1px_1px_rgba(0,0,0,0.05),0_4px_6px_rgba(34,42,53,0.04),0_24px_68px_rgba(47,48,55,0.05),0_2px_3px_rgba(0,0,0,0.04)] py-2 px-4">
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="size-4"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          </div>
+
+          <p className="text-xs">Aceternity</p>
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="size-4"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+              />
+            </svg>
+          </div>
+        </button>
       </div>
-    );
+
+      <div className="flex-1 bg-gray-100 mt-4 rounded-xl border border-dashed border-neutral-200 relative">
+        <motion.div
+          initial={{
+            opacity: 0,
+            scale: 0.98,
+             filter:  "blur(10px)"
+          }}
+
+          whileHover={{
+            opacity: 1,
+            scale: 1.05,
+            filter: 'blur(0px)'
+          }}
+          transition={{
+            duration: 0.4,
+            ease: easeInOut
+          }}
+          className="absolute inset-0 bg-white divide-y divide-neutral-200 rounded-xl h-full w-full border border-neutral-200">
+          <div className="p-4 flex gap-3">
+            <div className="size-7 shrink-0 shadow-[0_1px_1px_rgba(0,0,0,0.05),0_4px_6px_rgba(34,42,53,0.04),0_24px_68px_rgba(47,48,55,0.05),0_2px_3px_rgba(0,0,0,0.04)] rounded-md flex justify-center items-center bg-white bg-linear-to-br">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="size-4"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M13.5 16.875h3.375m0 0h3.375m-3.375 0V13.5m0 3.375v3.375M6 10.5h2.25a2.25 2.25 0 0 0 2.25-2.25V6a2.25 2.25 0 0 0-2.25-2.25H6A2.25 2.25 0 0 0 3.75 6v2.25A2.25 2.25 0 0 0 6 10.5Zm0 9.75h2.25A2.25 2.25 0 0 0 10.5 18v-2.25a2.25 2.25 0 0 0-2.25-2.25H6a2.25 2.25 0 0 0-2.25 2.25V18A2.25 2.25 0 0 0 6 20.25Zm9.75-9.75H18a2.25 2.25 0 0 0 2.25-2.25V6A2.25 2.25 0 0 0 18 3.75h-2.25A2.25 2.25 0 0 0 13.5 6v2.25a2.25 2.25 0 0 0 2.25 2.25Z"
+                />
+              </svg>
+            </div>
+            <div className="flex flex-col text-[0.6rem]">
+              <p className="font-bold text-neutral-600">
+                Aceternity UI components
+              </p>
+              <p className="text-neutral-400">
+                A collection of UI components which are hand made.
+              </p>
+            </div>
+          </div>
+
+          <div className="p-4 flex gap-3">
+            <div className="size-7 shrink-0 shadow-[0_1px_1px_rgba(0,0,0,0.05),0_4px_6px_rgba(34,42,53,0.04),0_24px_68px_rgba(47,48,55,0.05),0_2px_3px_rgba(0,0,0,0.04)] rounded-md flex justify-center items-center bg-white bg-linear-to-br">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="size-4"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 0 1 .865-.501 48.172 48.172 0 0 0 3.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z"
+                />
+              </svg>
+            </div>
+            <div className="flex flex-col text-[0.6rem]">
+              <p className="font-bold text-neutral-600">Aceternity Messages</p>
+              <p className="text-neutral-400">
+                All queries will be resolved through our trained staff,
+                available through chat and calls
+              </p>
+            </div>
+          </div>
+
+          <div className="p-4 flex gap-3">
+            <div className="size-7 shrink-0 shadow-[0_1px_1px_rgba(0,0,0,0.05),0_4px_6px_rgba(34,42,53,0.04),0_24px_68px_rgba(47,48,55,0.05),0_2px_3px_rgba(0,0,0,0.04)] rounded-md flex justify-center items-center bg-white bg-linear-to-br">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="size-4"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M3 8.689c0-.864.933-1.406 1.683-.977l7.108 4.061a1.125 1.125 0 0 1 0 1.954l-7.108 4.061A1.125 1.125 0 0 1 3 16.811V8.69ZM12.75 8.689c0-.864.933-1.406 1.683-.977l7.108 4.061a1.125 1.125 0 0 1 0 1.954l-7.108 4.061a1.125 1.125 0 0 1-1.683-.977V8.69Z"
+                />
+              </svg>
+            </div>
+            <div className="flex flex-col text-[0.6rem]">
+              <p className="font-bold text-neutral-600">24 Hours Turnaround.</p>
+              <p className="text-neutral-400">
+                All Queries will be resolved within 24 hours or you will get
+                refund immediately.
+              </p>
+            </div>
+          </div>
+        </motion.div>
+      </div>
+    </motion.div>
+  );
 }
